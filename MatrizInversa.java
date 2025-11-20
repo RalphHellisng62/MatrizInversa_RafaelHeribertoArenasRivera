e5dd0458ebaf0197dd80b8ed2315e605be396db2
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-19 21:13:38 -0600

    Construcción de main

--- MatrizInversa.java
+++ MatrizInversa.java
@@ -1,3 +1,26 @@
 public class MatrizInversa {
-    
+    // iniciamos con la construcción de un main
+    String archivoEntrada = "matriz.txt";
+        String archivoSalida = "matriz_inversa.txt";
+
+        double[][] matriz = leerMatriz(archivoEntrada);
+
+        if (matriz != null) {
+            System.out.println("matriz original:");
+            imprimirMatriz(matriz);
+
+            double[][] inversa = calcularInversa(matriz);
+
+            if (inversa != null) {
+                System.out.println("\n matriz inversa:");
+                imprimirMatriz(inversa);
+
+                escribirArchivo(inversa, archivoSalida);
+                System.out.println("\n archivo generado exitosamente: " + archivoSalida);
+            } else {
+                System.out.println("\n no se puede calcular la inversa (matriz no cuadrada o determinante = 0).");
+            }
+        } else {
+            System.out.println("error al leer la matriz.");
+        }
 }
